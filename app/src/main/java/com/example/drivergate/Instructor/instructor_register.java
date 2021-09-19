ad48c8358ef39da255565a46495d491384df366f
Sandeepa Viduranga <sviduranga41@gmail.com>
2021-09-19 15:57:27 +0530

    Updated version v1.0

---
+++ app/src/main/java/com/example/drivergate/Instructor/instructor_register.java
@@ -0,0 +1,14 @@
+package com.example.drivergate.Instructor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.drivergate.R;
+
+public class instructor_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_register);
+    }
+}
