df6aeff00951aaaaad2492c0566dc3103710c3bf
Ishanka Denipitiya <71344529+IshankaDenipitiya@users.noreply.github.com>
2021-10-17 03:12:06 +0530

    [feature] Add question feature implementation

--- app/src/main/java/com/example/drivergate/DrivingSchool/ds_add_questions.java
+++ app/src/main/java/com/example/drivergate/DrivingSchool/ds_add_questions.java
@@ -1,12 +1,22 @@
 package com.example.drivergate.DrivingSchool;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.example.drivergate.MainActivity;
 import com.example.drivergate.Modles.Questions;
 import com.example.drivergate.R;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -14,21 +24,26 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class ds_add_questions extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Button addQuestion;
-    private EditText Question, Answer1 , Answer2, Answer3, Answer4, CorrectAns;
+    private EditText Question, Answer1, Answer2, Answer3, Answer4;
+    private Spinner CorrectAns;
     private String ds_question, ds_answer1, ds_answer2, ds_answer3, ds_answer4, ds_correct;
     private FirebaseDatabase rootNode;
     private DatabaseReference reference;
+    AlertDialog.Builder builder;
+    boolean answerSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ds_add_questions);
 
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         Question = findViewById(R.id.question);
         Answer1 = findViewById(R.id.answer1);
@@ -38,32 +53,113 @@ public class ds_add_questions extends AppCompatActivity {
         CorrectAns = findViewById(R.id.correct_ans);
         addQuestion = findViewById(R.id.add_question);
 
-        reference= FirebaseDatabase.getInstance().getReference("Questions");
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.correctAnswer, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        CorrectAns.setAdapter(adapter);
+        CorrectAns.setPrompt("Correct Answer");
+        CorrectAns.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                spinnerChangeHandler();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        reference = FirebaseDatabase.getInstance().getReference("Questions");
         addQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    saveQuestions();
+                saveQuestions();
             }
         });
     }
-            private void saveQuestions(){
-                ds_question = Question.getText().toString();
-                ds_answer1 = Answer1.getText().toString();
-                ds_answer2 = Answer2.getText().toString();
-                ds_answer3 = Answer3.getText().toString();
-                ds_answer4 = Answer4.getText().toString();
-                ds_correct = CorrectAns.getText().toString();
 
-                Questions question =new Questions(ds_question,ds_answer1,ds_answer2,ds_answer3,ds_answer4,ds_correct);
-                String key= reference.push().getKey();
-                reference.child(key).setValue(question).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(ds_add_questions.this, "Data Inserted", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
+    private void saveQuestions() {
+        spinnerChangeHandler();
+        ds_question = Question.getText().toString().trim();
+        ds_answer1 = Answer1.getText().toString().trim();
+        ds_answer2 = Answer2.getText().toString().trim();
+        ds_answer3 = Answer3.getText().toString().trim();
+        ds_answer4 = Answer4.getText().toString().trim();
+
+        if (TextUtils.isEmpty(ds_question)) {
+            Question.setError("Please Enter a Question");
+            return;
+        }
+        if (TextUtils.isEmpty(ds_answer1)) {
+            Answer1.setError("Please Enter an Answer");
+            return;
+        }
+        if (TextUtils.isEmpty(ds_answer2)) {
+            Answer2.setError("Please Enter an Answer");
+            return;
+        }
+        if (TextUtils.isEmpty(ds_answer3)) {
+            Answer3.setError("Please Enter an Answer");
+            return;
+        }
+        if (TextUtils.isEmpty(ds_answer4)) {
+            Answer4.setError("Please Enter an Answer");
+            return;
+        }
+
+        if (!answerSelected) {
+            builder = new AlertDialog.Builder(ds_add_questions.this);
+            //Setting message manually and performing action on button click
+            builder.setMessage("Please select correct answer")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                        }
+                    });
+            //Creating dialog box
+            AlertDialog alert = builder.create();
+            //Setting the title & icon manually
+            alert.setTitle("Select Answer");
+            alert.show();
+        } else {
+
+            Questions question = new Questions(ds_question, ds_answer1, ds_answer2, ds_answer3, ds_answer4, ds_correct);
+            String key = reference.push().getKey();
+            reference.child(key).setValue(question).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(ds_add_questions.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ds_add_questions.this, ds_dashboard.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+    }
+
+    private void spinnerChangeHandler() {
+        int items = CorrectAns.getSelectedItemPosition();
+        switch (items) {
+            case 0:
+                answerSelected = false;
+                break;
+            case 1:
+                ds_correct = "ds_answer1";
+                answerSelected = true;
+                break;
+            case 2:
+                ds_correct = "ds_answer2";
+                answerSelected = true;
+                break;
+            case 3:
+                ds_correct = "ds_answer3";
+                answerSelected = true;
+                break;
+            case 4:
+                ds_correct = "ds_answer4";
+                answerSelected = true;
+                break;
+        }
+    }
 
     public void Go_back(View view) {
         Intent intent = new Intent(ds_add_questions.this, ds_dashboard.class);
--- app/src/main/java/com/example/drivergate/Student/user_questions_page.java
+++ app/src/main/java/com/example/drivergate/Student/user_questions_page.java
@@ -1,16 +1,50 @@
 package com.example.drivergate.Student;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import com.example.drivergate.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class user_questions_page extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    TextView questionNo, question, answer1, answer2, answer3, answer4;
+    RadioGroup questionGroup;
+    RadioButton answerRadioButton;
+    Button nextQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_questions_page);
+
+        questionNo = findViewById(R.id.questionNumber);
+        question = findViewById(R.id.question);
+        answer1 = findViewById(R.id.answer1);
+        answer2 = findViewById(R.id.answer2);
+        answer3 = findViewById(R.id.answer3);
+        answer4 = findViewById(R.id.answer4);
+        nextQuestion = findViewById(R.id.nextQuestion);
+
+        mAuth=FirebaseAuth.getInstance();
+        reference= FirebaseDatabase.getInstance().getReference("Questions");
+
+    }
+
+    public void checkAnswer(View view)
+    {
+        int radioId = questionGroup.getCheckedRadioButtonId();
+
+        answerRadioButton = findViewById(radioId);
+        //Toast.makeText(this, "Selected button :" + radioButton.getText(), Toast.LENGTH_SHORT).show();
     }
 }
